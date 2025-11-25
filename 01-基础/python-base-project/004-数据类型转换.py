d9e8a84e45de0203f25f566f9b9a4c5672aaa782
Supre-007-567 <Supre_567@outlook.com>
2025-11-25 20:24:29 +0800

    数据类型转化

---
+++ 01-基础/python-base-project/004-数据类型转换.py
@@ -0,0 +1,16 @@
+my_num = 100
+
+print("\n类型:", type(str(my_num)), "\n值:", str(my_num))
+
+my_str = "100"
+
+print("\n类型:", type(int(my_str)), "\n值:", int(my_num))
+
+"""
+int(11.11) -- 向下取整
+"""
+
+my_num2 = int(11.2)
+
+print("\n类型:", type(my_num2), "\n值:", my_num2)
+
