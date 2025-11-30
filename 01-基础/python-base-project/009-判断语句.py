8a1530c09754f94af693e7fe763c1aebd0ccc099
Supre-007-567 <Supre_567@outlook.com>
2025-11-30 21:52:21 +0800

    if elif else

--- 01-基础/python-base-project/009-判断语句.py
+++ 01-基础/python-base-project/009-判断语句.py
@@ -2,17 +2,28 @@
 判断语句
 """
 # 布尔类型
-result = 10 > 5
-print(f"结果: {result}\n类型: {type(result)}")
+# result = 10 > 5
+# print(f"结果: {result}\n类型: {type(result)}")
+#
+# while True:
+#     my_age = int(input("键入年龄\n"))
+#     if my_age < 0:
+#         print("请键入正确年龄")
+#         break
+#     if my_age >= 18:
+#         print("成年")
+#     else:
+#         print("未成年")
 
+"""
+if else
+"""
 while True:
-    my_age = int(input("键入年龄\n"))
-    if my_age < 0:
-        print("请键入正确年龄")
-        break
-    if my_age >= 18:
-        print("成年")
+    height = input("请输入您的身高(cm)\n")
+    if int(height) > 120:
+        print(f"身高{height}cm大于120cm，\n您需要支付10元购买门票\n祝您游玩愉快\n")
+    elif int(height) < 120:
+        print(f"身高{height}cm小于120cm，可以免费畅玩\n祝您游玩愉快\n")
     else:
-        print("未成年")
-
+        print("请输入正确身高")
 
