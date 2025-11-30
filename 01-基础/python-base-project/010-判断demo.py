4bb58f17b33d274d4e3d2ed7ca69195b68c86fc3
Supre-007-567 <Supre_567@outlook.com>
2025-11-30 22:59:50 +0800

    随机数+猜数字

---
+++ 01-基础/python-base-project/010-判断demo.py
@@ -0,0 +1,29 @@
+"""
+猜数字
+"""
+import random
+if 1:
+    right_num = random.randint(1, 10)
+    g_times = 0
+    print(f"猜数字游戏(right_num={right_num})")
+    while True:
+        my_num = int(input("请输入1-10的数字\n"))
+        g_times += 1
+        if my_num > 10:
+            print("请输入正确数字")
+            continue
+        elif my_num < 1:
+            print("请输入正确数字")
+        if my_num > right_num:
+            print("猜大了")
+        elif my_num < right_num:
+            print("猜小了")
+        else:
+            print(f"恭喜您仅用{g_times}次猜对了，就是{my_num}")
+            break
+        if g_times == 5:
+            print("次数耗尽")
+            break
+"""
+
+"""
