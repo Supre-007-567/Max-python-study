6c4355fa747f053ddaa82ef9986c5511f5c6ab67
Supre-007-567 <Supre_567@outlook.com>
2025-12-02 16:43:44 +0800

    优化99乘法表

--- 01-基础/python-base-project/012-循环demo.py
+++ 01-基础/python-base-project/012-循环demo.py
@@ -2,13 +2,12 @@
 99乘法表
 """
 i = 1
-j = 1
 while i <= 9:
     # print(f"i={i}")
+    j = 1
     while j <= i:
         print(f"{j} x {i} = {i * j}\t", end='')
         j += 1
     i += 1
-    j = 1
     print()
 
