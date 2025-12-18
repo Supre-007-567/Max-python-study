e7ba99d39f9cb998743aac4c36db0e9f6a0c977f
Supre-007-567 <Supre_567@outlook.com>
2025-12-18 16:38:45 +0800

    两种方法统计

--- 02-基础/python-base2-project/002-统计单词出现次数.py
+++ 02-基础/python-base2-project/002-统计单词出现次数.py
@@ -1,10 +1,13 @@
 """
 通过⽂件读取操作，读取此⽂件，统计itheima单词出现的次数
 """
-times = 0
+times1 = 0
+times2 = 0
 with open("C:/Users/ASUS/Desktop/曲高和寡/02-基础/file-python-txt2.txt","r",encoding="UTF-8") as f:
     for line in f:
-        print(type(line))
         if line.count("itheima"):
-            times += line.count("itheima")
-print(f"itheima出现了{times}次")
+            times2 += line.count("itheima")
+    times1 = f.read().count("itheima")
+
+print(f"方法①：\nitheima出现了{times1}次")
+print(f"方法②：\nitheima出现了{times2}次")
