4e8fdb1610c250dbcc234a77ff4fa357677284b0
Supre-007-567 <Supre_567@outlook.com>
2025-12-19 15:39:09 +0800

    包

---
+++ 02-基础/python-base2-project/007-包.py
@@ -0,0 +1,19 @@
+"""
+创建包
+
+"""
+# 导入自定义包中的模块并使用
+# import my_package.my_module1
+# import my_package.my_module2
+#
+# my_package.my_module1.print_info1()
+# my_package.my_module2.print_info2()
+
+# from my_package import my_module1
+# from my_package import my_module2
+# my_module1.print_info1()
+# my_module2.print_info2()
+
+from my_package import *
+my_module1.print_info1()
+
